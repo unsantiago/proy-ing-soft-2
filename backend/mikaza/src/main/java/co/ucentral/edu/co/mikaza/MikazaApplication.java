823e951c4cba982dce79039d845336ed5e844a8c
unsantiago <lprietoa2@gmail.com>
2024-04-09 09:23:18 -0500

    Este es el primer commit del proyecto - Leni

---
+++ backend/mikaza/src/main/java/co/ucentral/edu/co/mikaza/MikazaApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.edu.co.mikaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikazaApplication.class, args);
+	}
+
+}
